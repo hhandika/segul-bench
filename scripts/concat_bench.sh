89347b7ad6038366a5728890553d16020a374a7a
hhandika <herubiolog@gmail.com>
2022-02-28 20:44:52 -0600

    Fix iteration not displaying

--- scripts/concat_bench.sh
+++ scripts/concat_bench.sh
@@ -3,7 +3,6 @@
 set INPUT_DIR "shrew-nexus-clean-trimmed/"
 set OUTPUT_DIR "concat_results"
 set OUTPUT_LOG "concat_bench.log"
-set MESSAGE "Iteration $i"
 
 if test -f $OUTPUT_LOG
 rm $OUTPUT_LOG
@@ -14,7 +13,7 @@ echo "Benchmarking Alignment Concatenation"
 echo "Benchmarking SEGUL" >> $OUTPUT_LOG
 
 for i in (seq 10) do
-    echo $MESSAGE
+    echo "Iteration $i"
     env time -f "%E %M %P" segul concat -d $INPUT_DIR -f nexus -o $OUTPUT_DIR -F phylip-int 2>> $OUTPUT_LOG;
     rm -r $OUTPUT_DIR;
 end
@@ -23,7 +22,7 @@ echo "Benchmarking Phyluce" >> $OUTPUT_LOG
 conda activate phyluce
 
 for i in (seq 10) do
-    echo $MESSAGE
+    echo "Iteration $i"
     env time -f "%E %M %P" phyluce_align_concatenate_alignments --alignments $INPUT_DIR --output $OUTPUT_DIR --phylip 2>> $OUTPUT_LOG;
     rm -r $OUTPUT_DIR;
 end
