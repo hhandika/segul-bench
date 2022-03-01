fd25c342dc5aac01942070a9b83a1c879a868e66
hhandika <herubiolog@gmail.com>
2022-02-28 20:59:35 -0600

    Add first run to warm the cpu cache.

--- scripts/summary_bench.sh
+++ scripts/summary_bench.sh
@@ -8,17 +8,27 @@ if test -f $OUTPUT_LOG
     rm $OUTPUT_LOG
 end
 
+echo "Warming up..."
+
+segul summary -d $INPUT_DIR -f nexus -o $OUTPUT_DIR
+
 echo "Benchmarking Summary Statistics"
 
 echo "Benchmarking SEGUL" >> $OUTPUT_LOG
 for i in (seq 10) do
-    echo "Iteration $i"
-    env time -f "%E %M %P" segul summary -d $INPUT_DIR -f nexus -o $OUTPUT_DIR 2>> $OUTPUT_LOG;
     rm -r $OUTPUT_DIR;
+    echo "Iteration $i"
+    # We append the STDERR to the log file because gnu time output to STDERR
+    env time -f "%E %M %P" segul summary -d $INPUT_DIR -f nexus -o $OUTPUT_DIR 2>> $OUTPUT_LOG;
 end
 
-echo "Benchmarking Phyluce" >> $OUTPUT_LOG
 conda activate phyluce
+
+echo "Warming up..."
+
+phyluce_align_get_align_summary_data --alignments $INPUT_DIR --core 8
+
+echo "Benchmarking Phyluce" >> $OUTPUT_LOG
 
 for i in (seq 10) do
     echo "Iteration $i"
