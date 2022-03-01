8590ca0c5c86183a2b7ce1949678e773d4905df6
Heru Handika <herubiolog@gmail.com>
2022-02-28 23:46:47 -0600

    Add new lines

--- scripts/summary_bench_multidata.sh
+++ scripts/summary_bench_multidata.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set INPUT_DIR "shrew-nexus-clean-trimmed" "oliveros_2019_trimmed_80p" "jarvis_2014_uce_filtered_w_gator"
+set INPUT_DIRS "shrew-nexus-clean-trimmed" "oliveros_2019_trimmed_80p" "jarvis_2014_uce_filtered_w_gator"
 set OUTPUT_DIR "Summary_results"
 set OUTPUT_LOG "summary_bench.log"
 set CORES "24"
@@ -17,11 +17,11 @@ echo "Warming up..."
 
 segul summary -d "shrew-nexus-clean-trimmed/" -f nexus -o $OUTPUT_DIR
 
-echo "Benchmarking Summary Statistics"
+echo "\nBenchmarking Summary Statistics\n"
 
-echo "Benchmarking SEGUL" >> $OUTPUT_LOG
-for dir in $INPUT_DIR
-    echo "Using $dir dataset" >> $OUTPUT_LOG
+echo "Benchmarking SEGUL" | tee $OUTPUT_LOG
+for dir in $INPUT_DIRS
+    echo "Dataset path: $dir" | tee $OUTPUT_LOG
     for i in (seq 10)
         rm -r $OUTPUT_DIR;
         echo "Iteration $i"
@@ -36,14 +36,14 @@ if [ -d $OUTPUT_DIR ]
 rm -r $OUTPUT_DIR
 end
 
-echo "Warming up..."
+echo "\nWarming up...\n"
 
 phyluce_align_get_align_summary_data --alignments "shrew-nexus-clean-trimmed/" --core $CORES
 
-echo "Benchmarking Phyluce" >> $OUTPUT_LOG
+echo "Benchmarking Phyluce" | tee $OUTPUT_LOG
 
-for dir in $INPUT_DIR
-    echo "Using $dir dataset" >> $OUTPUT_LOG
+for dir in $INPUT_DIRS
+    echo "Dataset path: $dir" | tee $OUTPUT_LOG
     for i in (seq 10)
         echo "Iteration $i"
         env time -f "%E %M %P" phyluce_align_get_align_summary_data --alignments $dir --core $CORES 2>> $OUTPUT_LOG;
