2d4c0e294853e7a90dfc14c2ca7e9f190f90b37e
Heru Handika <herubiolog@gmail.com>
2022-03-05 21:33:18 -0600

    Add concatenation benchmark

--- scripts/summary_bench.sh
+++ scripts/summary_bench.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env fish
 
 set INPUT_DIRS "alignments/esselstyn_2021_nexus_trimmed" "alignments/oliveros_2019_80p_trimmed" "alignments/jarvis_2014_uce_filtered_w_gator"
-set OUTPUT_DIR "summary_results"
-set OUTPUT_LOG "summary_bench.txt"
+set OUTPUT_DIR "concat_results"
+set OUTPUT_LOG "data/concat_bench.txt"
 set CORES "24"
+
 
 # Get system information
 uname -v | tee -a $OUTPUT_LOG
@@ -16,44 +17,39 @@ if [ -d $OUTPUT_DIR ]
 rm -r $OUTPUT_DIR
 end
 
-echo "Warming up..."
+echo -e "Warming up..."
 
-segul summary -d "shrew-nexus-clean-trimmed/" -f nexus -o $OUTPUT_DIR
+segul concat -d alignments/esselstyn_2021_nexus_trimmed -f nexus -o $OUTPUT_DIR -F phylip
 
-echo "Benchmarking Summary Statistics"
+echo -e "\nBenchmarking Alignment Concatenation"
 
 echo "Benchmarking SEGUL" | tee -a $OUTPUT_LOG
 for dir in $INPUT_DIRS
+echo ""
 echo "Dataset path: $dir" | tee -a $OUTPUT_LOG
 for i in (seq 10)
 rm -r $OUTPUT_DIR;
+echo ""
 echo "Iteration $i"
 # We append the STDERR to the log file because gnu time output to STDERR
-env time -f "%E %M %P" segul summary -d $dir -f nexus -o $OUTPUT_DIR 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" segul concat -d $dir -f nexus -o $OUTPUT_DIR -F phylip 2>> $OUTPUT_LOG;
 end
 end
 
-conda activate phyluce
+### SEGUL ignore datatype ###
 
-if [ -d $OUTPUT_DIR ]
-rm -r $OUTPUT_DIR
-end
-
-echo "Warming up..."
-
-phyluce_align_get_align_summary_data --alignments "shrew-nexus-clean-trimmed/" --core $CORES
-
-echo "Benchmarking Phyluce" | tee -a $OUTPUT_LOG
-
+echo -e "Benchmarking SEGUL ignore datatype" | tee -a $OUTPUT_LOG
 for dir in $INPUT_DIRS
+echo ""
 echo "Dataset path: $dir" | tee -a $OUTPUT_LOG
 for i in (seq 10)
 rm -r $OUTPUT_DIR;
+echo ""
 echo "Iteration $i"
-env time -f "%E %M %P" phyluce_align_get_align_summary_data --alignments $dir --core $CORES 2>> $OUTPUT_LOG;
+# We append the STDERR to the log file because gnu time output to STDERR
+env time -f "%E %M %P" segul concat -i $dir/*.nex -f nexus -o $OUTPUT_DIR -F fasta-int --datatype ignore 2>> $OUTPUT_LOG;
 end
 end
-
 
 #### AMAS ####
 
@@ -66,7 +62,7 @@ end
 
 echo -e "\nWarming up..."
 
-AMAS.py summary -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -d dna -c $CORES
+AMAS.py concat -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -d dna -c $CORES
 
 echo -e "\nBenchmarking AMAS" | tee -a $OUTPUT_LOG
 
@@ -74,18 +70,44 @@ for dir in $INPUT_DIRS
 echo ""
 echo "Dataset path: $dir" | tee -a $OUTPUT_LOG
 for i in (seq 10)
-rm summary.txt
+rm concatenated.out && rm partitions.txt
 echo ""
 echo "Iteration $i"
-env time -f "%E %M %P" AMAS.py summary -i $dir/*.nex -f nexus -d dna -c $CORES 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" AMAS.py concat -i $dir/*.nex -f nexus -d dna -c $CORES 2>> $OUTPUT_LOG;
 end
 end
+
+#### Phyluce ####
+
+conda activate phyluce
+
+if [ -d $OUTPUT_DIR ]
+rm -r $OUTPUT_DIR
+end
+
+echo -e "\nWarming up..."
+
+phyluce_align_concatenate_alignments --alignments alignments/esselstyn_2021_nexus_trimmed --output $OUTPUT_DIR --phylip
+
+echo -e "\nBenchmarking Phyluce" | tee -a $OUTPUT_LOG
+
+for dir in $INPUT_DIRS
+echo ""
+echo "Dataset path: $dir" | tee -a $OUTPUT_LOG
+for i in (seq 10)
+rm -r $OUTPUT_DIR;
+echo ""
+echo "Iteration $i"
+env time -f "%E %M %P" phyluce_align_concatenate_alignments --alignments $dir --output $OUTPUT_DIR --phylip 2>> $OUTPUT_LOG;
+end
+end
+
 
 ### Final touches ###
 
 set Date (date +%F)
 
-set fname "summary_bench_raw_$Date.txt"
+set fname "concat_bench_raw_$Date.txt"
 
 mv $OUTPUT_LOG data/$fname
 
@@ -93,8 +115,9 @@ mv $OUTPUT_LOG data/$fname
 
 rm -r $OUTPUT_DIR
 rm *.log
-rm summary.txt
+rm concatenated.out && rm partitions.txt
 
 ### Push to Github ###
 
 git add -A && git commit -m "Add concatenation benchmark" && git push
+
