0d17725fda423e6afb226bccc8d4046eef0f0fd6
Heru Handika <herubiolog@gmail.com>
2022-03-06 00:06:21 -0600

    Add linux benchmark.

--- scripts/concat_bench_linux.sh
+++ scripts/concat_bench_linux.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env fish
 
 set INPUT_DIRS "alignments/esselstyn_2021_nexus_trimmed" "alignments/oliveros_2019_80p_trimmed" "alignments/jarvis_2014_uce_filtered_w_gator"
-set OUTPUT_DIR "summary_results"
-set OUTPUT_LOG "data/summary_bench.txt"
+set OUTPUT_DIR "concat_results"
+set OUTPUT_LOG "data/concat_bench.txt"
 set CORES "24"
 
 
 # Get system information
-uname -v >> $OUTPUT_LOG
+uname -v | tee -a $OUTPUT_LOG
 
 if test -f $OUTPUT_LOG
 rm $OUTPUT_LOG
@@ -19,9 +19,9 @@ end
 
 echo -e "Warming up..."
 
-segul summary -d alignments/esselstyn_2021_nexus_trimmed -f nexus -o $OUTPUT_DIR
+segul concat -d alignments/esselstyn_2021_nexus_trimmed -f nexus -o $OUTPUT_DIR -F phylip
 
-echo -e "\nBenchmarking Summary Stats"
+echo -e "\nBenchmarking Alignment Concatenation"
 
 echo "Benchmarking SEGUL" | tee -a $OUTPUT_LOG
 for dir in $INPUT_DIRS
@@ -32,7 +32,7 @@ rm -r $OUTPUT_DIR;
 echo ""
 echo "Iteration $i"
 # We append the STDERR to the log file because gnu time output to STDERR
-env time -f "%E %M %P" segul summary -i $dir/*.nex -f nexus -o $OUTPUT_DIR 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" segul concat -d $dir -f nexus -o $OUTPUT_DIR -F fasta-int 2>> $OUTPUT_LOG;
 end
 end
 
@@ -47,11 +47,13 @@ rm -r $OUTPUT_DIR;
 echo ""
 echo "Iteration $i"
 # We append the STDERR to the log file because gnu time output to STDERR
-env time -f "%E %M %P" segul summary -i $dir/*.nex -f nexus -o $OUTPUT_DIR --datatype ignore 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" segul concat -i $dir/*.nex -f nexus -o $OUTPUT_DIR -F fasta-int --datatype ignore 2>> $OUTPUT_LOG;
 end
 end
 
 #### AMAS ####
+
+conda activate pytools
 
 if [ -d $OUTPUT_DIR ]
 rm -r $OUTPUT_DIR
@@ -60,7 +62,7 @@ end
 
 echo -e "\nWarming up..."
 
-AMAS.py summary -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -d dna -c $CORES
+AMAS.py concat -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -d dna -c $CORES
 
 echo -e "\nBenchmarking AMAS" | tee -a $OUTPUT_LOG
 
@@ -68,10 +70,10 @@ for dir in $INPUT_DIRS
 echo ""
 echo "Dataset path: $dir" | tee -a $OUTPUT_LOG
 for i in (seq 10)
-rm summary.txt
+rm concatenated.out && rm partitions.txt
 echo ""
 echo "Iteration $i"
-env time -f "%E %M %P" AMAS.py summary -i $dir/*.nex -f nexus -d dna -c $CORES 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" AMAS.py concat -i $dir/*.nex -f nexus -d dna -c $CORES 2>> $OUTPUT_LOG;
 end
 end
 
@@ -85,7 +87,7 @@ end
 
 echo -e "\nWarming up..."
 
-phyluce_align_get_align_summary_data --alignments alignments/esselstyn_2021_nexus_trimmed --core $CORES
+phyluce_align_concatenate_alignments --alignments alignments/esselstyn_2021_nexus_trimmed --output $OUTPUT_DIR --phylip
 
 echo -e "\nBenchmarking Phyluce" | tee -a $OUTPUT_LOG
 
@@ -93,17 +95,19 @@ for dir in $INPUT_DIRS
 echo ""
 echo "Dataset path: $dir" | tee -a $OUTPUT_LOG
 for i in (seq 10)
+rm -r $OUTPUT_DIR;
 echo ""
 echo "Iteration $i"
-env time -f "%E %M %P" phyluce_align_get_align_summary_data --alignments $dir --core $CORES 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" phyluce_align_concatenate_alignments --alignments $dir --output $OUTPUT_DIR 2>> $OUTPUT_LOG;
 end
 end
+
 
 ### Final touches ###
 
 set Date (date +%F)
 
-set fname "summary_bench_raw_PC_$Date.txt"
+set fname "concat_bench_raw_$Date.txt"
 
 mv $OUTPUT_LOG data/$fname
 
@@ -111,9 +115,9 @@ mv $OUTPUT_LOG data/$fname
 
 rm -r $OUTPUT_DIR
 rm *.log
-rm summary.txt
+rm concatenated.out && rm partitions.txt
 
 ### Push to Github ###
 
-git add -A && git commit -m "Add summary benchmark" && git push
+git add -A && git commit -m "Add concatenation benchmark" && git push
 
