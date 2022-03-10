23ef41aabc99afe3139e548bdfbde5001a8c224c
Heru Handika <herubiolog@gmail.com>
2022-03-09 21:00:15 -0600

    Add concatenation benchmark

--- scripts/concat_bench_MacOS.sh
+++ scripts/concat_bench_MacOS.sh
@@ -1,12 +1,15 @@
 #!/opt/homebrew/bin/fish
 
-set INPUT_DIRS "alignments/esselstyn_2021_nexus_trimmed" "alignments/oliveros_2019_80p_trimmed" "alignments/jarvis_2014_uce_filtered_w_gator"
+set INPUT_DIRS "alignments/esselstyn_2021_nexus_trimmed" "alignments/oliveros_2019_80p_trimmed" "alignments/jarvis_2014_uce_filtered_w_gator" "alignments/chan_2020_loci/"
 set OUTPUT_DIR "concat_results"
 set OUTPUT_LOG "data/concat_bench.txt"
 set CORES 8
 
 # Get system information
-uname -v | tee -a $OUTPUT_LOG
+uname -v 2> $OUTPUT_LOG
+
+# Get segul version
+segul -V >> $OUTPUT_LOG
 
 if test -f $OUTPUT_LOG
 rm $OUTPUT_LOG
@@ -18,7 +21,7 @@ end
 
 echo -e "Warming up..."
 
-segul concat -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -o $OUTPUT_DIR -F fasta-int
+segul concat -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -o $OUTPUT_DIR -F phylip
 
 echo -e "\nBenchmarking Alignment Concatenation"
 
@@ -31,7 +34,7 @@ rm -r $OUTPUT_DIR;
 echo ""
 echo "Iteration $i"
 # We append the STDERR to the log file because gnu time output to STDERR
-gtime -f "%E %M %P" segul concat -i $dir/*.nex -f nexus -o $OUTPUT_DIR -F fasta-int 2>> $OUTPUT_LOG;
+gtime -f "%E %M %P" segul concat -i $dir/*.nex -f nexus -o $OUTPUT_DIR -F phylip 2>> $OUTPUT_LOG;
 end
 end
 
@@ -44,7 +47,7 @@ rm -r $OUTPUT_DIR;
 echo ""
 echo "Iteration $i"
 # We append the STDERR to the log file because gnu time output to STDERR
-gtime -f "%E %M %P" segul concat -i $dir/*.nex -f nexus -o $OUTPUT_DIR -F fasta-int --datatype ignore 2>> $OUTPUT_LOG;
+gtime -f "%E %M %P" segul concat -i $dir/*.nex -f nexus -o $OUTPUT_DIR -F phylip --datatype ignore 2>> $OUTPUT_LOG;
 end
 end
 
@@ -57,7 +60,7 @@ end
 
 echo -e "\nWarming up..."
 
-AMAS.py concat -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -d dna -c $CORES
+AMAS.py concat -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -d dna -u phylip -c $CORES
 
 echo -e "\nBenchmarking AMAS" | tee -a $OUTPUT_LOG
 
@@ -68,7 +71,7 @@ for i in (seq 10)
 rm concatenated.out && rm partitions.txt
 echo ""
 echo "Iteration $i"
-gtime -f "%E %M %P" AMAS.py concat -i $dir/*.nex -f nexus -d dna -c $CORES 2>> $OUTPUT_LOG;
+gtime -f "%E %M %P" AMAS.py concat -i $dir/*.nex -f nexus -d dna -u phylip -c $CORES 2>> $OUTPUT_LOG;
 end
 end
 
@@ -81,7 +84,7 @@ for i in (seq 10)
 rm concatenated.out && rm partitions.txt
 echo ""
 echo "Iteration $i"
-gtime -f "%E %M %P" AMAS.py concat -i $dir/*.nex -f nexus -d dna -c $CORES --check-align 2>> $OUTPUT_LOG;
+gtime -f "%E %M %P" AMAS.py concat -i $dir/*.nex -f nexus -d dna -u phylip -c $CORES --check-align 2>> $OUTPUT_LOG;
 end
 end
 
