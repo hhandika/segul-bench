3f472c4a62c02fadd9d77a2c7492b0137c60a8ae
Heru Handika <herubiolog@gmail.com>
2022-10-05 12:37:09 -0500

    Add script to bench other functions.

---
+++ scripts/other-features/bench_openSUSE.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env fish
+
+set INPUT_DIRS "alignments/esselstyn_2021_nexus_trimmed" "alignments/oliveros_2019_80p_trimmed" "alignments/jarvis_2014_uce_filtered_w_gator" "alignments/chan_2020_loci/"
+set OUTPUT_DIR "other_results"
+set OUTPUT_LOG_CONVERT "data/other_bench.txt"
+set CORES 24
+
+# Remove existing log file
+if test -f $OUTPUT_LOG
+rm $OUTPUT_LOG
+end
+
+# Get system information
+lscpu | egrep 'Model name|Thread|CPU\(s\)|Core\(s\) per socket' | tee -a $OUTPUT_LOG
+uname -r  | tee -a $OUTPUT_LOG
+
+# Get segul version
+segul -V | tee -a $OUTPUT_LOG
+
+if [ -d $OUTPUT_DIR ]
+rm -r $OUTPUT_DIR
+end
+
+
+echo -e "Warming up..."
+
+segul concat -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -o $OUTPUT_DIR -F phylip
+
+echo -e "\nBenchmarking Alignment Conversion..."
+
+echo "Benchmarking SEGUL Convert Nexus to Phylip" | tee -a $OUTPUT_LOG
+for dir in $INPUT_DIRS
+echo ""
+echo -e "\nDataset path: $dir" | tee -a $OUTPUT_LOG
+for i in (seq 10)
+rm -r $OUTPUT_DIR;
+echo ""
+echo "Iteration $i"
+# We append the STDERR to the log file because gnu time output to STDERR
+env time -f "%E %M %P" segul convert -i $dir/*.nex -f nexus -o $OUTPUT_DIR -F phylip 2>> $OUTPUT_LOG;
+end
+end
+
+### SEGUL ignore datatype ###
+
+echo -e "Benchmarking SEGUL ignore datatype" | tee -a $OUTPUT_LOG
+for dir in $INPUT_DIRS
+echo ""
+echo -e "\nDataset path: $dir" | tee -a $OUTPUT_LOG
+for i in (seq 10)
+rm -r $OUTPUT_DIR;
+echo ""
+echo "Iteration $i"
+# We append the STDERR to the log file because gnu time output to STDERR
+env time -f "%E %M %P" segul convert -i $dir/*.nex -f nexus -o $OUTPUT_DIR -F phylip --datatype ignore 2>> $OUTPUT_LOG;
+end
+end
+
+#### AMAS ####
+if [ -d $OUTPUT_DIR ]
+rm -r $OUTPUT_DIR
+end
+
+echo -e "\nWarming up..."
+
+AMAS.py convert -i alignments/esselstyn_2021_nexus_trimmed/*.nex -f nexus -d dna -u phylip -c $CORES
+
+echo -e "\nBenchmarking AMAS convert nexus to phylip" | tee -a $OUTPUT_LOG
+
+for dir in $INPUT_DIRS
+echo ""
+echo -e "\nDataset path: $dir" | tee -a $OUTPUT_LOG
+for i in (seq 10)
+rm *out.phy
+echo ""
+echo "Iteration $i"
+env time -f "%E %M %P" AMAS.py convert -i $dir/*.nex -f nexus -d dna -c $CORES -u phylip 2>> $OUTPUT_LOG;
+end
+end
+
+### AMAS Check Align ####
+
+echo -e "\nBenchmarking AMAS Check Align convert nexus to phylip" | tee -a $OUTPUT_LOG
+
+for dir in $INPUT_DIRS
+echo ""
+echo -e "\nDataset path: $dir" | tee -a $OUTPUT_LOG
+for i in (seq 10)
+rm *out.phy
+echo ""
+echo "Iteration $i"
+env time -f "%E %M %P" AMAS.py convert -i $dir/*.nex -f nexus -d dna -c $CORES -u phylip --check-align 2>> $OUTPUT_LOG;
+end
+end
+
+### Final touches ###
+
+set Date (date +%F)
+
+set fname "other_bench_raw_OpenSUSE_$Date.txt"
+
+mv $OUTPUT_LOG data/$fname
+
+### Cleaning up ###
+
+rm -r $OUTPUT_DIR
+rm *.log
+rm concatenated.out && rm partitions.txt
+
+### Push to Github ###
+
+git add -A && git commit -m "Add concatenation benchmark" && git push
+
