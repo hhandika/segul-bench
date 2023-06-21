9e0ae294182f57135dd1102593606404863ac3d2
Heru Handika <herubiolog@gmail.com>
2023-06-21 04:55:11 -0500

    Reorganize files.

--- scripts/run_linux.sh
+++ scripts/run_linux.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
-concat/concat_bench_openSUSE.sh
-concat/convert_bench_openSUSE.sh
-concat/concat_bench_AA_openSUSE.sh
-convert/convert_bench_aa_openSUSE.sh
-remove/remove_bench_openSUSE.sh
-split/split_bench_linux.sh
-summary/summary_bench_openSUSE.sh
-summary/summary_bench_AA_openSUSE.sh
+concat_bench_openSUSE.sh
+convert_bench_openSUSE.sh
+concat_bench_AA_openSUSE.sh
+convert_bench_aa_openSUSE.sh
+remove_bench_openSUSE.sh
+split_bench_linux.sh
+summary_bench_openSUSE.sh
+summary_bench_AA_openSUSE.sh
