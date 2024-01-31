38cadfa9427f45bac6d70fc7d69aa4318bb6bedb
Heru Handika <herubiolog@gmail.com>
2024-01-30 18:48:22 -0600

    Update scripts.

--- scripts/linux/split_bench_linux_AA.sh
+++ scripts/linux/split_bench_linux_AA.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env fish
 
 set INPUT_DIR "alignments/split_alignments"
-set INPUT_FILES "chan2020.nex" "esselstyn2021.nex" "jarvis2014.nex" "oliveros2019.nex" 
-set PARTITION "chan2020_partition.txt" "esselstyn2021_partition.txt" "jarvis2014_partition.txt" "oliveros2019_partition.txt" 
+set INPUT_FILES "shen2018.nex" "wu2018.nex"
+set PARTITION "shen2018_partition.txt" "wu2018_partition.txt"
 set OUTPUT_DIR "split_results"
 set OUTPUT_LOG "data/split_bench.txt"
 set CORES 24
@@ -31,7 +31,7 @@ end
 
 echo -e "Warming up..."
 
-segul align split -i $INPUT_DIR/chan2020.nex -f nexus -I $INPUT_DIR/chan2020_partition.txt -o $OUTPUT_DIR --output-format nexus
+segul align split -i $INPUT_DIR/shen2018.nex -f nexus -I $INPUT_DIR/shen2018_partition.txt -o $OUTPUT_DIR --output-format nexus --datatype aa
 
 echo -e "\nBenchmarking Alignment Splitting"
 
@@ -44,7 +44,7 @@ rm -r $OUTPUT_DIR;
 echo ""
 echo "Iteration $i"
 # We append the STDERR to the log file because gnu time output to STDERR
-env time -f "%E %M %P" segul align split -i $INPUT_DIR/$file -f nexus -I $INPUT_DIR/${PARTITION[$file]} -p raxml -o $OUTPUT_DIR --output-format nexus 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" segul align split -i $INPUT_DIR/$file -f nexus -I $INPUT_DIR/${PARTITION[$file]} -p raxml -o $OUTPUT_DIR --output-format nexus --datatype aa 2>> $OUTPUT_LOG;
 end
 
 ### SEGUL ignore datatype ###
@@ -69,7 +69,7 @@ end
 
 
 echo -e "\nWarming up..."
-AMAS.py split -i $INPUT_DIR/chan2020.nex -f nexus -d dna -l $INPUT_DIR/chan2020_partition.txt -d dna -u nexus -c $CORES
+AMAS.py split -i $INPUT_DIR/shen2018.nex -f nexus -d aa -l $INPUT_DIR/shen2018_partition.txt -d aa -u nexus -c $CORES
 
 echo -e "\nBenchmarking AMAS (--remove-empty)" | tee -a $OUTPUT_LOG
 for file in $INPUT_FILES
@@ -77,10 +77,10 @@ echo ""
 echo "Dataset path: $file" | tee -a $OUTPUT_LOG
 
 for i in (seq $NUM_ITERATIONS)
-rm alignments/chan_2020_all_combined/alignment_all-combined_*
+rm alignments/split_alignments/$file_*
 echo ""
 echo "Iteration $i"
-env time -f "%E %M %P" AMAS.py split -i $INPUT_DIR/$file -f nexus -d dna -l $INPUT_DIR/${PARTITION[$file]} -u nexus --remove-empty -c $CORES 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" AMAS.py split -i $INPUT_DIR/$file -f nexus -d aa -l $INPUT_DIR/${PARTITION[$file]} -u nexus --remove-empty -c $CORES 2>> $OUTPUT_LOG;
 end
 
 ### AMAS with empty sequences ###
@@ -90,10 +90,10 @@ for file in $INPUT_FILES
 echo ""
 echo "Dataset path: $file" | tee -a $OUTPUT_LOG
 for i in (seq $NUM_ITERATIONS)
-rm alignments/chan_2020_all_combined/alignment_all-combined_*
+rm alignments/split_alignments/$file_*
 echo ""
 echo "Iteration $i"
-env time -f "%E %M %P" AMAS.py split -i $INPUT_DIR/$file -f nexus -d dna -l $INPUT_DIR/${PARTITION[$file]} -u nexus -c $CORES 2>> $OUTPUT_LOG;
+env time -f "%E %M %P" AMAS.py split -i $INPUT_DIR/$file -f nexus -d aa -l $INPUT_DIR/${PARTITION[$file]} -u nexus -c $CORES 2>> $OUTPUT_LOG;
 end
 
 ### Final touches ###
