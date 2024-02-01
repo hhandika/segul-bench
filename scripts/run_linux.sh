63af0d33932f9706414eed947251579d6bea2377
Heru Handika <herubiolog@gmail.com>
2024-01-31 22:02:41 -0600

    Linux runs.

--- scripts/run_linux.sh
+++ scripts/run_linux.sh
@@ -1,2 +1,18 @@
-#!/usr/bin/env fish
+#!/usr/bin/envfish
 
+concat-fasta_bench_AA_openSUSE.sh
+concat-fasta_bench_openSUSE.sh
+concat-phylip_bench_AA_openSUSE.sh
+concat-phylip_bench_openSUSE.sh
+convert-fasta_bench_aa_openSUSE.sh
+convert-fasta_bench_openSUSE.sh
+convert-phylip_bench_aa_openSUSE.sh
+convert-phylip_bench_openSUSE.sh
+remove-fasta_bench_AA_openSUSE.sh
+remove-fasta_bench_openSUSE.sh
+remove-phylip_bench_AA_openSUSE.sh
+remove-phylip_bench_openSUSE.sh
+summary-fasta_bench_AA_openSUSE.sh
+summary-fasta_bench_openSUSE.sh
+summary-phylip_bench_AA_openSUSE.sh
+summary-phylip_bench_openSUSE.sh
