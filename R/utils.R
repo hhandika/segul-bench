0e57c4fa4af11765172e16b8713c03f4104e9d44
Heru Handika <herubiolog@gmail.com>
2024-02-01 11:14:03 -0600

    Switch app to symbols.

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
 # Color blind friendly palette
 # Define matching paletter for each programs.
-accessible_palette <- c("#949df0", "#4e49c1", "#aa48af", "#76a146", "#c5c140", "#984531", "#c7a488")
-point_shapes <- c(15, 16, 17, 18, 0, 1)
+accessible_palette <- c("#949df0", "#4e49c1", "#aa48af", "#76a146", "#c5c140", "#984531", "#c7a488", "#000000")
+point_shapes <- c(15, 18, 17, 3, 4, 0, 2)
