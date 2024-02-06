8bef0a6301caa91fc14a0945b463e3a42bc07672
Heru Handika <herubiolog@gmail.com>
2024-02-06 15:56:25 -0600

    Add more colors.

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,24 @@
 # Color blind friendly palette
 # Define matching paletter for each programs.
 accessible_palette <- c("#949df0", "#4e49c1", "#aa48af", "#76a146", "#c5c140", "#984531", "#c7a488", "#000000")
-point_shapes <- c(15, 18, 17, 3, 4, 0, 2)
+point_shapes <- c(15, 18, 17, 3, 4, 0, 2, 6, 8)
+
+ai_pastel_palette <- c("#00bf7d", "#00b4c5", "#0073e6", "#2546f0", "#5928ed", "#b3c7f7", "#8babf1", "#0461cf")
+
+ai_pastel_palette_2 <- c("#e6308a", "#5ba300", "#89ce00", "#0073e6", "#c44601", "#f57600")
+
+ai_orange <- c("#999999", "#e6308a", "#5ba300", "#89ce00", "#0073e6", "#c44601")
+
+ai_forest <- c("#59621D", "#4E801F", "#D7C826", "#5F4029", "#CDA890", "#D6D2C4")
+
+ai_avocado <- c("#356211", "#558303", "#6C8F32", "#F2E880", "#C15C37", "#AA471F")
+
+ai_2024 <- c("#B76552", "#5F7C55", "#5B8DBD", "#E78B77", "#5C5F8D", "#2BB58C")
+
+ai_2024_4 <- c("#5F7C55", "#5B8DBD", "#E78B77", "#2BB58C")
+
+ai_contrast <- c("#5DA5D1", "#FF5733", "#FFC300", "#DAF7A6", "#C70039", "#900C3F")
+
+mountain_sunset_palette <- c("#001B55", "#356211", "#FDB694", "#FD7942", "#E4581D", "#FC9642")
+
+sunset_mountains_palette <- c("#4B557D", "#536A99", "#727FA9", "#F2D4BD", "#F6B396", "#B68A99")
